8d60e0b3d0cd872fb8aa60e38ae8ecd34193d24a
sheep <samcoxe0307@gmail.com>
2023-12-06 10:19:36 +0700

    BaiTap

--- test.js
+++ test.js
@@ -2,6 +2,6 @@
 var Pi=3.14
 let C=(2*R*Pi)
 console.log(C);
-document.write(C +"<br>")
-document.write(R*R*Pi)
+document.write(" Chu vi :" + C +"<br>")
+document.write(" Diện tích : " + R * R * Pi )
 }
